3d7b72b966a31cd1f1b92c97df771e6ca612b4f7
Oscar Dartigues <oscardartigues92@gmail.com>
2023-02-03 16:33:01 +0100

    implementing colors selection

--- exo.py
+++ exo.py
@@ -12,26 +12,39 @@ password= 'scarboule'
 wlan.connect(ssid, password)
 url= "http://192.168.43.109:3000/"
 
+red_btn = Pin(10, mode=Pin.IN),
+blue_btn = Pin(11, mode=Pin.IN),
+green_btn = Pin(12, mode=Pin.IN),
+yellow_btn = Pin(13, mode=Pin.IN)
 
 all_led = [Pin(17, mode=Pin.OUT),Pin(18, mode=Pin.OUT),Pin(20, mode=Pin.OUT),Pin(22, mode=Pin.OUT)]
-btn = Pin(15, mode=Pin.IN, pull=Pin.PULL_UP)
+btn_reset = Pin(15, mode=Pin.IN, pull=Pin.PULL_UP)
+color_btn = [
+    red_btn,
+    blue_btn,
+    green_btn,
+    yellow_btn
+]
 
 
 while not wlan.isconnected():
     time.sleep(1)
     print("noco")
 
-while(True):
-    print(url + "reset/")
-    print(btn.value())
-    if btn.value() == 1:
-        r = urequests.get(url + "reset")
-        result = r.json()
-        print(result)
-    time.sleep(0.1)
+
+
+while True:
+    if btn_reset.value() == 0:
+        r = urequests.get('http://192.168.43.109:3000/reset')
+        print(r.json())
+        r.close()
+        print("press")
+    print("null")
+
     try:
         for i in range(len(all_led)):
             all_led[i].off()
+        print("request get")
         r = urequests.get(url)
         result = r.json()
         print(result)
@@ -40,6 +53,8 @@ while(True):
                 all_led[i].on()
             
         r.close()
-        time.sleep(0.5)
+        time.sleep(2)
     except Exception as e:
         print(e)
+
+
